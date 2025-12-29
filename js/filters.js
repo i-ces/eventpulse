f89134b15162f3a33fecc2a0d770f75ed2c2f517
baby <baby@aelu.moon>
2025-12-29 21:50:22 +0545

    adds filterByCategory function

--- js/filters.js
+++ js/filters.js
@@ -2,3 +2,19 @@
  * EventPulse - Filters Module
  * Handles filtering and searching of events
  */
+
+/**
+ * Filters events by category
+ * @param {Array} eventsArray - Array of events to filter
+ * @param {string} category - Category to filter by (empty string for all)
+ * @returns {Array} Filtered array of events
+ */
+function filterByCategory(eventsArray, category) {
+  if (!category || category === 'all') {
+    return eventsArray;
+  }
+  
+  return eventsArray.filter(event => 
+    event.category.toLowerCase() === category.toLowerCase()
+  );
+}
