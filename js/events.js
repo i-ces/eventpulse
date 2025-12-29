d80d6c4a03e8df664f6cf12bb4a918949035aa57
baby <baby@aelu.moon>
2025-12-29 21:56:25 +0545

    adds toggleSaveEvent function

--- js/events.js
+++ js/events.js
@@ -14,3 +14,22 @@ let appState = {
     savedOnly: false
   }
 };
+
+/**
+ * Toggles the saved state of an event
+ * @param {string} eventId - The ID of the event to toggle
+ */
+function toggleSaveEvent(eventId) {
+  const eventIndex = appState.events.findIndex(e => e.id === eventId);
+  
+  if (eventIndex === -1) return;
+  
+  // Toggle the saved state
+  appState.events[eventIndex].isSaved = !appState.events[eventIndex].isSaved;
+  
+  // Update the UI
+  updateSaveButton(eventId, appState.events[eventIndex].isSaved);
+  
+  // Persist to localStorage
+  saveStateToLocalStorage({ events: appState.events });
+}
